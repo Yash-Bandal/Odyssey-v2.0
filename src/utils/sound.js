ff52d76d5228e3c20f1102ca1df4e5f48d100c0d
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2026-02-20 23:26:51 +0530

    Add files via upload

---
+++ src/utils/sound.js
@@ -0,0 +1,28 @@
+import clickSound from "../assets/click-sound.mp3"
+import pauseSound from "../assets/pause.wav"
+import dropDownSound from "../assets/dropDown.mp3"
+
+const clickAudio = new Audio(clickSound)
+const pauseAudio = new Audio(pauseSound)
+const dropDownAudio = new Audio(dropDownSound)
+
+
+clickAudio.volume = 0.5
+pauseAudio.volume = 0.5
+dropDownAudio.volume = 0.5
+
+
+export const playClick = () => {
+    clickAudio.currentTime = 0
+    clickAudio.play()
+}
+
+export const playPause = () => {
+    pauseAudio.currentTime = 0
+    pauseAudio.play()
+}
+
+export const playDropDown = () => {
+    dropDownAudio.currentTime = 0
+    dropDownAudio.play()
+}
