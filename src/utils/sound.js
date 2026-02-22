fb321a1757dc4b5be7f1c6cc3914680f96b0d696
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2026-02-22 23:43:03 +0530

    Update sound.js

--- src/utils/sound.js
+++ src/utils/sound.js
@@ -1,15 +1,19 @@
 import clickSound from "../assets/click-sound.mp3"
-import pauseSound from "../assets/pauseF.mp3"
+import pauseSound from "../assets/pause.wav"
 import dropDownSound from "../assets/dropDown.mp3"
+import deleteSound from "../assets/delete.mp3"
 
 const clickAudio = new Audio(clickSound)
 const pauseAudio = new Audio(pauseSound)
 const dropDownAudio = new Audio(dropDownSound)
+const deleteAudio = new Audio(deleteSound)
 
 
 clickAudio.volume = 0.5
 pauseAudio.volume = 0.5
 dropDownAudio.volume = 0.5
+deleteAudio.volume = 0.5
+
 
 
 export const playClick = () => {
@@ -25,4 +29,8 @@ export const playPause = () => {
 export const playDropDown = () => {
     dropDownAudio.currentTime = 0
     dropDownAudio.play()
+}
+export const playDelete = () => {
+    deleteAudio.currentTime = 0
+    deleteAudio.play()
 }
