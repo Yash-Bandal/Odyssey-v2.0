ac506c584c1ee6311496bcc41849c9c18855db1a
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2026-04-01 12:29:06 +0530

    Update sound.js

--- src/utils/sound.js
+++ src/utils/sound.js
@@ -1,44 +1,94 @@
-import clickSound from "../assets/click-sound.mp3"
-import pauseSound from "../assets/pause.wav"
-import dropDownSound from "../assets/dropDown.mp3"
-import deleteSound from "../assets/delete.mp3"
+//======= Audio context
+let audioContext
+let buffers = {}
 
-const clickAudio = new Audio(clickSound)
-const pauseAudio = new Audio(pauseSound)
-const dropDownAudio = new Audio(dropDownSound)
-const deleteAudio = new Audio(deleteSound)
+//  INIT (load + decode once)
+export const initAudio = async () => {
+    if (audioContext) return
 
+    audioContext = new (window.AudioContext || window.webkitAudioContext)()
 
-clickAudio.volume = 0.5
-pauseAudio.volume = 0.5
-dropDownAudio.volume = 0.5
-deleteAudio.volume = 0.5
+    const loadSound = async (name, url) => {
+        const res = await fetch(url)
+        const arrayBuffer = await res.arrayBuffer()
+        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer)
+        buffers[name] = audioBuffer
+    }
 
-
-
-export const playClick = () => {
-    clickAudio.currentTime = 0
-    clickAudio.play()
+    await Promise.all([
+        loadSound("click", "/sounds/click-sound.mp3"),
+        loadSound("pause", "/sounds/pause.wav"),
+        loadSound("dropdown", "/sounds/dropDown.mp3"),
+        loadSound("delete", "/sounds/delete.mp3"),
+    ])
 }
 
-export const playPause = () => {
-    pauseAudio.currentTime = 0
-    pauseAudio.play()
+//  INTERNAL PLAY FUNCTION
+const play = (name, volume = 0.5) => {
+    if (!audioContext || !buffers[name]) return
+
+    const source = audioContext.createBufferSource()
+    const gainNode = audioContext.createGain()
+
+    source.buffer = buffers[name]
+    gainNode.gain.value = volume
+
+    source.connect(gainNode)
+    gainNode.connect(audioContext.destination)
+
+    source.start(0)
 }
 
-export const playDropDown = () => {
-    dropDownAudio.currentTime = 0
-    dropDownAudio.play()
-}
-export const playDelete = () => {
-    deleteAudio.currentTime = 0
-    deleteAudio.play()
-}
+// EXPORTED PLAY FUNCTIONS (use these everywhere)
+export const playClick = () => play("click")
+export const playPause = () => play("pause")
+export const playDropDown = () => play("dropdown")
+export const playDelete = () => play("delete")
 
 
-export const initSounds = () => {
-    clickAudio.load()
-    pauseAudio.load()
-    dropDownAudio.load()
-    deleteAudio.load()
-}
+//==========================
+//====================== before 1 april 2025, new Audio
+// import clickSound from "../assets/click-sound.mp3"
+// import pauseSound from "../assets/pause.wav"
+// import dropDownSound from "../assets/dropDown.mp3"
+// import deleteSound from "../assets/delete.mp3"
+
+// const clickAudio = new Audio(clickSound)
+// const pauseAudio = new Audio(pauseSound)
+// const dropDownAudio = new Audio(dropDownSound)
+// const deleteAudio = new Audio(deleteSound)
+
+
+// clickAudio.volume = 0.5
+// pauseAudio.volume = 0.5
+// dropDownAudio.volume = 0.5
+// deleteAudio.volume = 0.5
+
+
+
+// export const playClick = () => {
+//     clickAudio.currentTime = 0
+//     clickAudio.play()
+// }
+
+// export const playPause = () => {
+//     pauseAudio.currentTime = 0
+//     pauseAudio.play()
+// }
+
+// export const playDropDown = () => {
+//     dropDownAudio.currentTime = 0
+//     dropDownAudio.play()
+// }
+// export const playDelete = () => {
+//     deleteAudio.currentTime = 0
+//     deleteAudio.play()
+// }
+
+
+// export const initSounds = () => {
+//     clickAudio.load()
+//     pauseAudio.load()
+//     dropDownAudio.load()
+//     deleteAudio.load()
+// }
